1d19bf8b7546539b3005ac5e02068091ba79df1f
Jazz2407 <jaibharath2407@gmail.com>
2026-03-08 13:47:47 +0530

    Fix: Add vercel.json for routing, update CSP, and sync skill categories

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from "./app/App";
 import "./app/styles/index.css";
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
